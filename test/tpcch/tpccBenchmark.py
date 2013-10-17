fb412dace94ca73935aa9b783c2703c494c53345
cfrahnow <clemens24@web.de>
2013-10-17 16:58:46 +0200

    improved benchmark script

--- test/tpcch/tpccBenchmark.py
+++ test/tpcch/tpccBenchmark.py
@@ -4,14 +4,30 @@ import subprocess
 import os
 import sys
 
+successCounter = 0
+queryCounter = 0
+
 def execute(queryName):
+	global queryCounter
+	global successCounter
+
+	queryCounter += 1
 	sys.stdout.write('Executing '+queryName+'.. ')
 	sys.stdout.flush()
 	try:
-		os.system('curl -X POST --data-urlencode "query@'+queryName+'.json" http://localhost:5000/jsonQuery > /dev/null')
+		os.system('curl -X POST --data-urlencode "query@'+queryName+'.json" http://localhost:5000/jsonQuery')
+		print('Success')
+		successCounter += 1
+
 	except Exception as err:
-	    print(queryName+' failed: '+ str(runner.std_err))
-	    exit(1)
-	print('Success')
+	    print(queryName+' failed: '+ str(err))
+	    
+	print('')
+
 
 execute('query1')
+execute('query6')
+
+print('');
+print('Executed queries: '+str(queryCounter))
+print('Errors: '+str(queryCounter-successCounter))
