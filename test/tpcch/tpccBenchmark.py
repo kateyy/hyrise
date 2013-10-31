0ab823f36c134cf22fa251f7093d9791c79225a8
Clemens Frahnow <clemens24@web.de>
2013-10-31 20:10:50 +0100

    improve benchmark and query1 + add query19

--- test/tpcch/tpccBenchmark.py
+++ test/tpcch/tpccBenchmark.py
@@ -4,18 +4,28 @@ import subprocess
 import os
 import sys
 
+# counts number of successful executed queries
 successCounter = 0
+# counts number of executed queries
 queryCounter = 0
 
 def execute(queryName):
+	filePath = os.path.dirname(__file__)+'/'+queryName+'.json'
+
+	# do not execute not existing queries
+	if not os.path.exists(filePath):
+		return
+
 	global queryCounter
 	global successCounter
 
 	queryCounter += 1
 	sys.stdout.write('Executing '+queryName+'.. ')
 	sys.stdout.flush()
+
+	# executing
 	try:
-		os.system('curl -X POST --data-urlencode "query@'+queryName+'.json" http://localhost:5000/jsonQuery')
+		os.system('curl -X POST --data-urlencode "query@'+filePath+'" http://localhost:5000/jsonQuery')
 		print('Success')
 		successCounter += 1
 
@@ -24,10 +34,10 @@ def execute(queryName):
 	    
 	print('')
 
-
-execute('query1')
-execute('query6')
+# executing query 1-22
+for i in range(1,23):
+	execute('query'+str(i))
 
 print('');
-print('Executed queries: '+str(queryCounter))
+print('Executed queries: '+str(queryCounter)+'/22')
 print('Errors: '+str(queryCounter-successCounter))
