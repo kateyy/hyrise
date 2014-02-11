8bb97b28bcd0f20033d0655dcbf1c6fc82f3ea75
Clemens Frahnow <clemens24@web.de>
2014-02-11 16:33:31 +0100

    comment mod files

--- src/lib/access/Mod.h
+++ src/lib/access/Mod.h
@@ -7,6 +7,24 @@
 namespace hyrise {
 namespace access {
 
+/// This class implements the mod function as a hyrise operation
+/// You can set the divisor and the field that should be affected
+/// This operation will create a new column containing the result values
+/// Usage:
+/// ........
+///   "mod" :{
+///       "type" : "Mod",
+///       "fields" : ["C_ID"],
+///       "vtype" : 0,
+///       "divisor" : 10,
+///       "as" : "MOD_FIELD"
+///   },
+/// ........
+/// "fields" contains the field that acts as divident (should only have one element)
+/// "vtype" should define the value type of the operation (int = 0, float = 1)
+/// "divisor" is the number, that devides the given field
+/// "as" contains the name of the new column
+
 class Mod : public ParallelizablePlanOperation {
 public:
   virtual ~Mod();
